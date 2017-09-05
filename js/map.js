80991782641728d5bdded359460168bd5526fe9a
elenagradovich <volozhinskaya@gmail.com>
2017-09-05 21:35:56 +0300

    Добавлена реакцию на перемещение (drag) пина заполняемого объета

--- js/map.js
+++ js/map.js
@@ -3,6 +3,7 @@
 (function () {
   var mapMarks = document.querySelector('.tokyo__pin-map');
   var fragment = document.createDocumentFragment();
+  var inputAddress = document.getElementById('address');
 
   for (var j = 0; j < 8; j++) {
     fragment.appendChild(window.pin.getMapMarks(window.data.rentedAccommodations[j]));
@@ -39,5 +40,46 @@
     if (evt.keyCode === ESC_KEYCODE && window.pin.activePin) {
       window.card.closeDialogPanel();
     }
+  });
+
+  var pinCurentAd = document.querySelector('.pin__main');
+
+  pinCurentAd.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+    var startingPinPosition = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+      var shift = {
+        x: startingPinPosition.x - moveEvt.clientX,
+        y: startingPinPosition.y - moveEvt.clientY
+      };
+
+      startingPinPosition = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      pinCurentAd.style.top = (pinCurentAd.offsetTop - shift.y) + 'px';
+      pinCurentAd.style.left = (pinCurentAd.offsetLeft - shift.x) + 'px';
+      var pinAddress = {
+        y: pinCurentAd.offsetTop - shift.y - window.pin.PIN_HEIGHT,
+        x: pinCurentAd.offsetLeft - shift.x - (window.pin.PIN_WIDTH / 2)
+      };
+      var pinPosition = 'x: ' + pinAddress.x + ', y: ' + pinAddress.y;
+      inputAddress.setAttribute('value', pinPosition);
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
   });
 })();
